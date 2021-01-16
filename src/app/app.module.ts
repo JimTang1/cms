363b496a1428a7e6fcdc202be63bfdc867afbeb0
JimTang <after062111@byui.edu>
2021-01-16 01:34:29 -0700

    cms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactPersonComponent } from './contacts/contact-list/contact-person/contact-person.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { DocumentEditComponent } from './documents/document-edit/document-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    HeaderComponent,
+    ContactDetailComponent,
+    ContactListComponent,
+    ContactPersonComponent,
+    DocumentsComponent,
+    DocumentEditComponent
   ],
   imports: [
     BrowserModule
