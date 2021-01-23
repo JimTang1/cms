4da7b19e94830c292ca935dbcd31e37145e3215a
JimTang1 <after062111@gmail.com>
2021-01-23 11:37:22 -0700

    import message.model correctly

--- src/app/messages/message.model.ts
+++ src/app/messages/message.model.ts
@@ -1,7 +1,7 @@
 export class Message{
     constructor(
         public id: string,
-        public suject: string,
+        public subject: string,
         public msgText: string,
         public sender: string
     ){}
